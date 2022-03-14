de3cd8e902e5cbcaf64c9f5c73e1e101bac0b4b5
sloppy-cat <js3891129@gmail.com>
2022-03-14 09:30:16 +0900

    project init

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import Search from './components/search/Search.vue';
+
+import { onMounted, ref, watch } from 'vue';
+const {keyword, setKeyword, enterKeyword, handleSubmit} = useKeyword()
+
+watch(enterKeyword, ()=>{
+  console.log('a')
+})
+
+
+function useKeyword() {
+  const keyword = ref<string>('')
+  const enterKeyword = ref<string>('')
+  function setKeyword(e: InputEvent) {
+    keyword.value = e.target?.value
+  }
+  function handleSubmit(e: SubmitEvent) {
+    enterKeyword.value = e.target?.value
+  }
+  return {keyword, setKeyword, enterKeyword, handleSubmit}
+}
+
+
+
+
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Search :keyword="keyword" :setKeyword="setKeyword" :enterKeyword="enterKeyword" :handleSubmit="handleSubmit" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/search/Search.vue
@@ -0,0 +1,38 @@
+<template lang="">
+  <div>
+
+    <input type="text" :value="keyword" @input="setKeyword" @change="handleSubmit" >
+    <p>k - {{keyword}}</p>
+    <p>fk - {{enterKeyword}}</p>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+import { onMounted, ref, watch } from 'vue';
+const {keyword, setKeyword, enterKeyword, handleSubmit} = defineProps(['keyword', 'setKeyword', 'enterKeyword', 'handleSubmit'])
+
+onMounted(()=>{
+})
+// watch(enterKeyword, ()=>{
+//   console.log('a')
+// })
+
+
+// function useKeyword() {
+//   const keyword = ref<string>('')
+//   const enterKeyword = ref<string>('')
+//   console.log(keyword.value)
+//   function setKeyword(e: InputEvent) {
+//     keyword.value = e.target?.value
+//   }
+//   function handleSubmit(e: SubmitEvent) {
+//     enterKeyword.value = e.target?.value
+//   }
+//   return {keyword, setKeyword, enterKeyword, handleSubmit}
+// }
+
+</script>
+<style lang="">
+  
+</style>
